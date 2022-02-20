7acedd164af49e0ee23350066aa54c3c703612c9
Ryan Hines <39021120+hines-r@users.noreply.github.com>
2022-02-20 00:45:00 -0600

    Updates

---
+++ precache-manifest.6f5a57f5dbab33f4c7856f03b7d7cef5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9340e9df5d1a163a1403a5f0e229501c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65bf380bd324421dfaac",
+    "url": "/static/css/main.c3227376.chunk.css"
+  },
+  {
+    "revision": "0679b9d09ab20d1d5f04",
+    "url": "/static/js/2.5b078481.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.5b078481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65bf380bd324421dfaac",
+    "url": "/static/js/main.d1c65637.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "e075c801eda47ec038c49921ac6797a6",
+    "url": "/static/media/fitbit.e075c801.png"
+  },
+  {
+    "revision": "30b4b88f7250d8ec82aa4c2832cf2a80",
+    "url": "/static/media/kaomojicon.30b4b88f.png"
+  },
+  {
+    "revision": "d4659bf46c4ca4a33c109d00e954fb2e",
+    "url": "/static/media/passmaid.d4659bf4.png"
+  },
+  {
+    "revision": "62dd1447218ab15c20ab07751e8c69e0",
+    "url": "/static/media/pixelchaos.62dd1447.gif"
+  }
+]);
